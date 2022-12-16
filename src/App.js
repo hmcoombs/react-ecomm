d32565b82a963aa5d773ecc4b4753e4f7b4ee219
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-12-15 21:04:33 -0500

    chg route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./components/Cart";
 import ProductMainPage from "./components/ProductMainPage";
